ced96b2fb56fbd6a02d14c1101cc63cb4d6ae2fa
AntonKC <ayk@ooott.ru>
2018-06-09 22:05:47 +0300

    Create dummy

--- game.js
+++ game.js
@@ -21,6 +21,7 @@ class Vector {
     }
 }
 
+// "Абстрактный" класс, из которого будем ваять все подвижные элементы в игре
 class Actor {
     constructor(position = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
         if ((!(position instanceof Vector)) || (!(size instanceof Vector)) || (!(speed instanceof Vector))) {
@@ -141,15 +142,158 @@ class Level {
     }
 }
 
-// Болванка, чтобы отображалась схема уровня
-class Player extends Actor {
-    //
+class LevelParser {
+    constructor(dictionary = {}) {
+        this.dictionary = Object.assign({}, dictionary);
+    }
+
+    actorFromSymbol(symbol) {
+        return this.dictionary[symbol];
+    }
+
+    obstacleFromSymbol(symbol) {
+        if (symbol === 'x') {
+            return 'wall';
+        }
+        else if (symbol === '!') {
+            return 'lava';
+        }
+    }
+
+    createGrid(plan) {
+        return plan.map(el => el.split('').map(item => this.obstacleFromSymbol(item)));
+    }
+
+    createActors(plan) {
+        return plan.reduce((memo, el, i) => {
+            el.split('').forEach((item, j) => {
+                const makeActor = this.actorFromSymbol(item);
+                    if (typeof makeActor === 'function') {
+                        const obj = new makeActor(new Vector(j, i));
+                        if (obj instanceof Actor) {
+                            memo.push(obj);
+                            return memo;
+                        }
+                    }
+            });
+            return memo;
+        }, []);
+    }
+
+    parse(plan) {
+        return new Level(this.createGrid(plan), this.createActors(plan));
+    }
 }
 
-// Отобразить схему уровня
-const grid = [
-    new Array(3),
-    ['wall', 'wall', 'lava']
-];
-const level = new Level(grid);
-runLevel(level, DOMDisplay);
+// "Абстрактный" класс для фаерболов или фаерскэров =)
+class Fireball extends Actor {
+    constructor(position = new Vector(0, 0), speed = new Vector(0, 0)) {
+        super(position, new Vector(1, 1), speed);
+    }
+
+    get type() {
+        return 'fireball';
+    }
+
+    getNextPosition(time = 1) {
+        return this.pos.plus(this.speed.times(time));
+    }
+
+    handleObstacle() {
+        this.speed = this.speed.times(-1);
+    }
+
+    act(time, level) {
+        const nextPos = this.getNextPosition(time);
+        const obstacle = level.obstacleAt(nextPos, this.size);
+        if (obstacle) {
+            this.handleObstacle();
+        } else {
+            this.pos = nextPos;
+        }
+    }
+}
+
+class HorizontalFireball extends Fireball {
+    constructor(position = new Vector(0, 0)) {
+        super(position, new Vector(2, 0));
+    }
+}
+
+class VerticalFireball extends Fireball {
+    constructor(position = new Vector(0, 0)) {
+        super(position, new Vector(0, 2));
+    }
+}
+
+// Самая бесячая штука
+class FireRain extends Fireball {
+    constructor(position = new Vector(0, 0)) {
+        super(position, new Vector(0, 3));
+        this.initialpos = position;
+    }
+
+    // Отличительная особенность - возврат на исходную позицию (эффект падающего дождя)
+    handleObstacle() {
+        this.pos = this.initialpos;
+    }
+}
+
+// Деньги на основе все того же Actor
+class Coin extends Actor {
+    constructor(position = new Vector(0, 0)) {
+        super(position.plus(new Vector(0.2, 0.1)), new Vector(0.6, 0.6));
+        this.initialpos = this.pos;
+        this.spring = Math.random() * (2 * Math.PI);
+        this.springSpeed = 8;
+        this.springDist = 0.07;
+    }
+
+    get type() {
+        return 'coin';
+    }
+
+    updateSpring(time = 1) {
+        this.spring += this.springSpeed * time;
+    }
+
+    getSpringVector() {
+        return new Vector(0, Math.sin(this.spring) * this.springDist);
+    }
+
+    getNextPosition(time = 1) {
+        this.updateSpring(time);
+        return this.initialpos.plus(this.getSpringVector());
+    }
+
+    act(time) {
+        this.pos = this.getNextPosition(time);
+    }
+}
+
+// Сам игрок
+class Player extends Actor {
+    constructor(position = new Vector(0, 0)) {
+        super(position.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5), new Vector(0, 0));
+    }
+
+    get type() {
+        return 'player';
+    }
+}
+
+const schemas = loadLevels();
+
+const actorDict = {
+    //'v': FireRain, // бесит - первый уровень не пройти
+    '@': Player,
+    '=': HorizontalFireball,
+    'o': Coin,
+    '|': VerticalFireball
+}
+
+const parser = new LevelParser(actorDict);
+
+schemas.then(result => {
+    runGame(JSON.parse(result), parser, DOMDisplay).then(() => alert('Вы выиграли приз!'));
+});
